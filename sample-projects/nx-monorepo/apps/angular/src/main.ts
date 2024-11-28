a024b7a315fc466cc87a45919d2440c4c06393ba
Wei <weilinzung@gmail.com>
2024-11-28 18:50:11 -0500

    fix: update error check

--- sample-projects/nx-monorepo/apps/angular/src/main.ts
+++ sample-projects/nx-monorepo/apps/angular/src/main.ts
@@ -3,8 +3,13 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 import { boo2 } from './test';
+import { booAbc } from './test-2';
 
+// This should error because it is possible undefined
 console.log(boo2);
+
+// This line is intentionally ignored by strict type-checking rules - "@ts-strict-ignore"
+console.log(booAbc);
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
--- sample-projects/nx-monorepo/apps/angular/src/test-2.ts
+++ sample-projects/nx-monorepo/apps/angular/src/test-2.ts
@@ -1,8 +1,8 @@
-// leave only: @ts-strict to see a strict error
+// @ts-strict-ignore
 interface TestType {
   bar: string;
 }
 
 const foo1: TestType | undefined = undefined;
 
-export const boo2 = foo1.bar;
+export const booAbc = foo1.bar;
